bdde71f2ec3467f6bc681556838b19d2906b6447
Whisper <mrromansimonov@gmail.com>
2022-11-19 12:26:35 +0500

    added classes

---
+++ RegisterOfCatchingWorkSchedules/Role.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegisterOfCatchingWorkSchedules
+{
+    public class Role
+    {
+        public string Name { get; private set; }
+        public RecordStatus[] Powers { get; private set; }
+    }
+}
---
+++ RegisterOfCatchingWorkSchedules/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegisterOfCatchingWorkSchedules
+{
+    public class User
+    {
+        public string Login { get; set; }
+        private string Password { get; set; }
+        public Role Role { get; private set; }
+        public Organisation Organisation { get; private set; }
+        public Municipality Municipality { get; private set; }
+    }
+}
