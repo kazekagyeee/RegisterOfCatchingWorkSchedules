ed45b985e9d374cb82ad3ae0a5957fcccb71f97c
Whisper <mrromansimonov@gmail.com>
2022-12-10 16:13:50 +0500

    Дополнен RegisterDBContext

--- RegisterOfCatchingWorkSchedules/RegisterDBContext.cs
+++ RegisterOfCatchingWorkSchedules/RegisterDBContext.cs
@@ -17,5 +17,17 @@ namespace RegisterOfCatchingWorkSchedules
 		public DbSet<Places> Places { get; set; }
 
 		public DbSet<Municipality> Municipality { get; set;}
+
+		public DbSet<Organisation> Organisation { get; set; }
+
+		public DbSet<Users> Users { get; set; }
+
+		public DbSet<Statuses> Statuses { get; set; }
+
+		public DbSet<StatusHistory> StatusHistory { get; set; }
+
+		public DbSet<RolePowers> RolePowers { get; set; }
+
+		public DbSet<Records> Records { get; set; }
 	}
 }
