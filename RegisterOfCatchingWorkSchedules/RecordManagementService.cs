e0997ac4d597a8b7399fea5f25ecb3082db0ddfc
Whisper <mrromansimonov@gmail.com>
2022-12-10 18:45:33 +0500

    добавил сервис менеджмента записей

---
+++ RegisterOfCatchingWorkSchedules/RecordManagementService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegisterOfCatchingWorkSchedules
+{
+    public class RecordManagementService
+    {
+        public Records GetRecords(int planID)
+        {
+            return Program.DBContext.Records.FirstOrDefault(x => x.RecordPlanID == planID);
+        }
+
+        public void CreateRecord(int placeID, int planID, DateTime date)
+        {
+            var record = new Records();
+            record.PlaceID = placeID;
+            record.RecordPlanID = planID;
+            record.RecordDate = date;
+            Program.DBContext.Records.Add(record);
+        }
+
+        public void DeleteRecord(int recordID)
+        {
+            var recordToDelete = Program.DBContext.Records.FirstOrDefault(x => x.ID == recordID);
+            Program.DBContext.Records.Remove(recordToDelete);
+        }
+    }
+}
