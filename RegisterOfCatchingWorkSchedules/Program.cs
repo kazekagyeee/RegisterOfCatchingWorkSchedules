a8574d8c29c8f3a1d927b0bbcce96b23ee47d20e
Scoolnik <62171058+Scoolnik@users.noreply.github.com>
2022-12-11 02:14:40 +0500

    add more test data

--- RegisterOfCatchingWorkSchedules/Program.cs
+++ RegisterOfCatchingWorkSchedules/Program.cs
@@ -23,15 +23,25 @@ namespace RegisterOfCatchingWorkSchedules
             DBContext = new RegisterDBContext();
             Session = new SessionService();
 
-			//var tmn = DBContext.Municipality.Add(new Municipality() { MunicipalityName = "Тюмень" });
-			//DBContext.Municipality.Add(new Municipality() { MunicipalityName = "Тобольск" });
-			//DBContext.Municipality.Add(new Municipality() { MunicipalityName = "Ишим" });
+            //var tmn = DBContext.Municipality.Add(new Municipality() { MunicipalityName = "Тюмень" });
+            //DBContext.Municipality.Add(new Municipality() { MunicipalityName = "Тобольск" });
+            //DBContext.Municipality.Add(new Municipality() { MunicipalityName = "Ишим" });
 
-   //         for (int i = 1; i <= 6; i++)
-   //             tmn.Places.Add(new Places() { PlacesName = $"{i} мкр" });
-			//DBContext.SaveChanges();
+            //         for (int i = 1; i <= 6; i++)
+            //             tmn.Places.Add(new Places() { PlacesName = $"{i} мкр" });
 
-			Application.Run(new MainForm());
+            //var tmn = DBContext.Municipality.Single(x => x.ID == 1);
+            //         var str = "Автоград Антипино Бабарынка Березняковский Богандинский Б.Тараскуль Букино Быкова Ватутина Верхний_Бор Войновка Воровского Воронино Восточный Гилево Гилевское Кольцо Гольф-палас Городище Дербыши ДОК Дом_Обороны Дударева Завод_медоборудования Зайково Зарека Заречный Затюменка Казарово Княжева Комарово Копытово КПД Матмасы Маяк Метелева МЖК ММС Московский дворик Московский_п. Московский_тр. М.Тараскуль Мыс Нобель Ново-Патрушево Ожогина Очаково Парфеново Патрушева Патрушево Плеханово Привоз Промзона Утешево Рабочий_поселок Рощино РТС СМП Стрела Суходолье Тараскуль Тарманы Тура_(Лесобаза) ТЭЦ_1 Тюменская слобода Утешево Учхоз Центр Червишевский_тр. Электрон Югра Южный_мкр Ялуторовского_тр.,_11_км Ямальский-2 Ямальский_мкр";
+            //         var places = str.Split(' ');
+            //         foreach (var place in places)
+            //         {
+            //	tmn.Places.Add(new Places() { PlacesName = place.Replace('_', ' ') });
+            //}
+
+            DBContext.SaveChanges();
+
+
+            Application.Run(new MainForm());
         }
     }
 }
