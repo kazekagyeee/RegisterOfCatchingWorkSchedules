b5d19d75d18a71bea1083b22202d303e743ff94d
Whisper <mrromansimonov@gmail.com>
2022-12-10 17:07:51 +0500

    Начал делать сервис по показу планов

---
+++ RegisterOfCatchingWorkSchedules/PlansManagementService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegisterOfCatchingWorkSchedules
+{
+    public class PlansManagementService
+    {
+        public List<Plans> GetAllowedPlans()
+        {
+            var currentUser = Program.Session.User;
+            var userRoles = currentUser.Roles;
+            /*if (currentUser != null)
+            {
+                var powers = Program.DBContext.RolePowers
+                    .Select(x => x.RoleID == currentUser.ID).ToList();
+            }*/
+
+            throw new NotImplementedException();
+        }
+    }
+}
