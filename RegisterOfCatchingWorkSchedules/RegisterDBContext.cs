f8de9a2a94470cb88cde354b074a65686cfc094c
Scoolnik <62171058+Scoolnik@users.noreply.github.com>
2022-12-10 16:03:17 +0500

    добавил DbContext

---
+++ RegisterOfCatchingWorkSchedules/RegisterDBContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RegisterOfCatchingWorkSchedules
+{
+	public class RegisterDBContext : DbContext
+	{
+		public DbSet<Plans> Plans { get; set; }
+
+		public DbSet<Roles> Roles { get; set; }
+
+		public DbSet<Places> Places { get; set; }
+
+		public DbSet<Municipality> Municipality { get; set;}
+	}
+}
