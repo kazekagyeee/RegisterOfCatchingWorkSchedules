099398b7369d04e63ada02b6248f756b2e34da84
Whisper <mrromansimonov@gmail.com>
2022-12-11 15:29:08 +0500

    добавил сервис статусов

---
+++ RegisterOfCatchingWorkSchedules/services/StatusesService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegisterOfCatchingWorkSchedules.services
+{
+    public static class StatusesService
+    {
+        public static BindingList<Statuses> GetBindingList()
+        {
+            using (var dbContext = new RegisterDBContext())
+            {
+                dbContext.Statuses.Load();
+                return dbContext.Statuses.Local.ToBindingList();
+            }
+            
+        }
+    }
+}
