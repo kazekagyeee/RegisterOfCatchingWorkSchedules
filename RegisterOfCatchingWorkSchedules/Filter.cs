0bfd53bf839acbdfb2e14565f70eb3d79034c92f
Scoolnik <62171058+Scoolnik@users.noreply.github.com>
2022-12-17 15:07:11 +0500

    сделал фильтр

--- RegisterOfCatchingWorkSchedules/Filter.cs
+++ RegisterOfCatchingWorkSchedules/Filter.cs
@@ -1,6 +1,17 @@
-﻿namespace RegisterOfCatchingWorkSchedules
+﻿using System;
+
+namespace RegisterOfCatchingWorkSchedules
 {
 	public class Filter
 	{
+		public DateTime MinPlanDate { get; set; }
+		public DateTime MaxPlanDate { get; set;}
+
+		public int StatusID { get; set; }
+
+		public int MunicipalityID { get; set; }
+
+		public DateTime MinPlanStatusChangeDate { get; set; }
+		public DateTime MaxPlanStatusChangeDate { get; set; }
 	}
 }
